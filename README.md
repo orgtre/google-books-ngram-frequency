8e16017414d88aa75cba348416ba877a9135889a
orgtre <orgtre@posteo.net>
2022-08-29 00:40:46 +0200

    Add code for creating first version of cleaned n-grams
    Also add the cleaned lists of n-grams.

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Google Books n-gram frequency lists
 
-*Note: Lists and code follow soon.*
-
 This repository provides word and n-gram frequency lists for all the Google books corpora that can be searched with the [Google Books Ngram Viewer](https://books.google.com/ngrams/). The source data is version 20200217 of the n-gram lists made available by Google [here](https://storage.googleapis.com/books/ngrams/books/datasetsv3.html).
 
 The full list of the corpora for which n-gram frequency lists are provided is: English, English Fiction, Chinese (simplified), French, German, Hebrew, Italian, Russian, and Spanish. In the provided lists the corpora are restricted to books published in the years 2010-2019.
@@ -11,10 +9,23 @@ Customizable Python code which reproduces the lists is also included.
 The content of this repository is licensed under the [Creative Commons Attribution 3.0 Unported License](https://creativecommons.org/licenses/by/3.0/).
 
 
+## Lists with n-grams
+
+The lists with the most frequent n-grams for each language and n from 1 to 5 are found in the `ngrams` directory. The lenght of these lists varies with n and language; for example, the 10.000 most common words (1-grams) are provided for each language.
+
+The lists found directly in the `ngrams` directory have been cleaned and are intended for use when developing language-learning materials. The sub-directory `ngrams/more` contains uncleaned and less cleaned versions which might be of use for e.g. linguists. The lists suffixed "removed" can be used to check what words are excluded from the final cleaned lists.
+
+
 ## Python code
 
 Optionally, start by running `create_source_data_lists.py` from the repository root directory to recreate the `source-data` folder with lists of links to the Google source data files.
 
 Run `download_and_extract_most_freq.py` from the repository root directory to dowload each file listed in `source-data` (a ".gz-file") and extract the most frequent n-grams in it into a list saved in `ngrams/more/{lang}/most_freq_ngrams_per_gz_file`. To save computer resources each .gz-file is immediately deleted after this. Since the lists of most frequent n-grams per .gz-file still take up around 36GB with the default settings, only one example list is uploaded to Github: `ngrams/more/english/most_freq_ngrams_per_gz_file/ngrams_1-00006-of-00024.gz.csv`. No cleaning has been performed at this stage, so this is how the raw data looks.
 
-TODO upload code to gather lists of the most frequent n-grams across all .gz-files and clean them.
+Run `gather_and_clean.py`to gather all the n-grams into lists of the overall most frequent ones and clean these lists by excluding unwanted n-grams.
+
+
+## Limitations and known problems
+
+The n-gram lists still contain entries which should be excluded, and, especially for n larger than one, some n-grams have been wrongfully excluded. Hence the lists are still subject to change. Version tags will be introduced once they are somewhat stable.
+
--- python/gather_and_clean.py
+++ python/gather_and_clean.py
@@ -1,4 +1,5 @@
 
+from cmath import nan
 import pandas as pd
 import numpy as np
 import os
@@ -15,8 +16,7 @@ year_start = 2010
 year_end = 2019
 
 # List of languages for which frequency lists should be extracted
-langs = ["chinese_simplified", "english", "english-fiction", "french", "german", 
-         "hebrew", "italian", "russian", "spanish"]
+langs = ["chinese_simplified", "english", "english-fiction", "french", "german", "hebrew", "italian", "russian", "spanish"]
 # each element has to be one of "english", "english-us", "english-gb", 
 # "english-fiction", "chinese_simplified", "french", "german", 
 # "hebrew", "italian", "russian", or "spanish"
@@ -27,25 +27,28 @@ number_of_most_freq = {"chinese_simplified": {1: 10000, 2: 5000, 3: 3000, 4: 100
                        "english-fiction":    {1: 10000, 2: 5000, 3: 3000, 4: 1000, 5: 1000},
                        "french":             {1: 10000, 2: 5000, 3: 3000, 4: 1000, 5: 1000},
                        "german":             {1: 10000, 2: 5000, 3: 3000, 4: 1000, 5: 1000},
-                       "hebrew":             {1: 10000, 2: 5000, 3: 2500, 4: 1000, 5:  400},
+                       "hebrew":             {1: 10000, 2: 5000, 3: 1000, 4: 200, 5:  80},
                        "italian":            {1: 10000, 2: 5000, 3: 3000, 4: 1000, 5: 1000},
                        "russian":            {1: 10000, 2: 5000, 3: 3000, 4: 1000, 5: 1000},
                        "spanish":            {1: 10000, 2: 5000, 3: 3000, 4: 1000, 5: 1000}}
 
 # Number of most frequent ngrams to keep per file
-per_file_number_of_most_freq = {"chinese_simplified": {1: 25000, 2: 25000, 3: 25000, 4: 25000, 5: 25000},
+per_file_number_of_most_freq = {"chinese_simplified": {1: 40000, 2: 25000, 3: 25000, 4: 25000, 5: 25000},
                                 "english":            {1: 25000, 2: 5000, 3: 3000, 4: 3000, 5: 5000},
-                                "english-fiction":    {1: 25000, 2: 5000, 3: 3000, 4: 3000, 5: 5000},
+                                "english-fiction":    {1: 40000, 2: 10000, 3: 3000, 4: 3000, 5: 5000},
                                 "french":             {1: 25000, 2: 5000, 3: 3000, 4: 3000, 5: 5000},
-                                "german":             {1: 25000, 2: 5000, 3: 3000, 4: 3000, 5: 10000},
-                                "hebrew":             {1: 25000, 2: 25000, 3: 25000, 4: 25000, 5: 25000},
-                                "italian":            {1: 25000, 2: 5000, 3: 3000, 4: 3000, 5: 5000},
+                                "german":             {1: 25000, 2: 5000, 3: 3000, 4: 10000, 5: 20000},
+                                "hebrew":             {1: 50000, 2: 50000, 3: 50000, 4: 50000, 5: 50000},
+                                "italian":            {1: 25000, 2: 5000, 3: 3000, 4: 3000, 5: 10000},
                                 "russian":            {1: 25000, 2: 25000, 3: 25000, 4: 25000, 5: 25000},
                                 "spanish":            {1: 25000, 2: 5000, 3: 3000, 4: 3000, 5: 5000}}
 
 
 ###############################################################################
 # Constants etc.
+
+all_langs = ["chinese_simplified", "english", "english-fiction", "french", "german", 
+         "hebrew", "italian", "russian", "spanish"]
 
 langcode = {"english": "eng", "english-us": "eng-us", "english-gb": "eng-gb", 
             "english-fiction": "eng-fiction", "chinese_simplified": "chi_sim", 
@@ -75,6 +78,8 @@ def per_gz_file_path(lang):
 
 def check_if_too_much_truncated(lang, n, d):
 
+    global max_min_freq_per_file
+    
     if d.shape[0] < number_of_most_freq[lang][n]:
 
         raise Exception("Error: Not enough rows in outfile.\nNumber of rows in outfile:", d.shape[0], 
@@ -146,7 +151,322 @@ def clean_remove_pos_tags(lang, n, d):
     return d
 
 
-def gather_and_clean():
+def split_contractions(d, dother):
+    '''Split contractions in the ngram column of 'd' append the resulting 
+    larger n-grams to the corresponding dataframe in 'dother'.
+    Examples of contractions are qu'il and c'est.
+    Return a touple of the modified 'd' and 'dother'.'''
+
+    dt = d.ngram.str.split(" ", expand=True)
+    n = dt.shape[1]
+
+    dt2s = []
+    for i in range(dt.shape[1]):
+        dt2s.append(dt[i].str.split(r"(.*?')", expand=True))
+    dt2 = pd.concat(dt2s, axis=1)
+    dt2 = dt2.replace("", None)
+
+    idx = (dt2.values == None).argsort(axis=1)
+    dt3 = pd.DataFrame(
+        dt2.values[np.arange(dt2.shape[0])[:, None], idx],
+        index=dt2.index,
+        columns=dt2.columns)
+
+    nwords = dt3.apply(lambda x: (~x.isnull()).sum(), axis='columns')
+
+    dt4 = dt3.fillna(value="")
+    dt4 = dt4.apply(' '.join, axis=1)
+    dt4 = dt4.apply(lambda x: (x.strip()))
+
+    d.ngram = dt4
+
+    # add the ngrams which now have other than n words to dother
+    for i in set(np.unique(nwords).tolist()) - {n}:
+        dother[i] = pd.concat([dother[i], d[nwords == i]])
+
+    d = d[nwords == n]
+
+    return d, dother
+
+
+def split_2grams_to_1grams(d2):
+
+    dt = d2.ngram.str.split(" ", expand=True)
+    n = dt.shape[1]
+
+    dt = dt.stack(dropna=False)
+    dt = dt.reset_index(level=[0,1], drop=True)
+
+    df = pd.DataFrame(np.repeat(d2.freq.values, n, axis=0))
+
+    dt = pd.concat([dt, df], axis=1)
+    dt.columns = ["ngram", "freq"]
+
+    return dt
+
+
+def merge_upcase_lowcase(d, cutoff):
+    '''Combine words with different capitalization. 
+    E.g. il - Il, le - Le, je - Je, etc.
+    Merges capitalized entries to non-capitalized ones unless 
+    the share capitalized is at least cutoff.
+    cutoff = 0.92 is a good value for French.
+    For German this works too.'''
+
+    first_letter = d['ngram'].str[:1]
+    later_letters = d['ngram'].str[1:]
+    dup = d[first_letter.str.isupper()]
+    dup['ngramlow'] = first_letter[first_letter.str.isupper()].str.lower() + later_letters[first_letter.str.isupper()]
+    # merging it like this ignores words that are all cap, since they'd cause problems later and are few enough to ignore
+
+    del first_letter
+    del later_letters
+
+    dup = dup.rename(columns={"ngram": "ngramup", "freq": "frequp"})
+
+    dup = pd.merge(dup, d, left_on=["ngramlow"], right_on=["ngram"])
+    del dup['ngramlow']
+
+    dup['shareup'] = dup.frequp / (dup.frequp + dup.freq)
+    # dup.sort_values(by=['shareup'], ascending=False).to_csv(f"{tmp_path(lang)}/ngrams_{n}_tmp.csv", index=False)
+    # if above cutoff remain large, otherwise merge with small
+
+
+    # merging this properly
+
+    dup['freq'] += dup['frequp']
+
+    upcase_to_remove = dup[dup.shareup < cutoff][['ngramup']]
+    lowcase_to_remove = dup[dup.shareup >= cutoff][['ngram']]
+    freqs_to_update = dup[['ngram', 'freq']]
+
+    del dup
+
+
+    # removes rows matched from upcase_to_remove
+
+    d = d.merge(upcase_to_remove, left_on=['ngram'], right_on=['ngramup'],
+                    how='left', indicator=True)
+    d = d[d._merge == "left_only"]
+    del d['_merge']
+    del d['ngramup']
+    del upcase_to_remove
+
+
+    # removes rows matched from lowcase_to_remove
+
+    d = d.merge(lowcase_to_remove, left_on=['ngram'], right_on=['ngram'],
+                    how='left', indicator=True)
+    d = d[d._merge == "left_only"]
+    del d['_merge']
+    del lowcase_to_remove
+
+
+    # update freqs
+
+    d = d.merge(freqs_to_update, on=['ngram'], how='left')
+    d['freq'] = d['freq_y'].fillna(d['freq_x']).astype('int')
+    d = d.drop(['freq_x', 'freq_y'], axis=1)
+    d = d.sort_values(by=['freq'], ascending=False).reset_index(drop=True)
+
+    del freqs_to_update
+
+    return d
+
+
+
+def remove_pattern(d, pattern, exceptions=[]):
+    '''Look for regex 'pattern' and remove matching ngrams,
+    except if they are in list 'exceptions'.'''
+    global freq_words_removed
+    if exceptions != []:
+        freq_words_removed += d[(d.ngram.str.contains(pattern)) & (~d['ngram'].isin(exceptions))].freq.sum()
+        d = d[(~d.ngram.str.contains(pattern)) | (d['ngram'].isin(exceptions))]
+    else:
+        freq_words_removed += d[d.ngram.str.contains(pattern)].freq.sum()
+        d = d[~d.ngram.str.contains(pattern)]
+    return d
+
+
+def replace_pattern_and_group(d, pattern, replacement):
+    '''Replace 'pattern' in d.ngram with 'replacement'. 
+    Group resulting entries by the value of d.ngram and 
+    add frequencies within groups.'''
+    d.ngram = d.ngram.str.replace(pattern, replacement, regex=True)
+    d = d.groupby("ngram")[["freq"]].sum()
+    d.reset_index(level=0, inplace=True)
+    d = d.sort_values(by=['freq'], ascending=False).reset_index(drop=True)
+    return d
+
+def remove_entries(d, entries):
+    '''Remove 'entries', updating 'freq_words_removed'.'''
+    global freq_words_removed
+    freq_words_removed += d[d['ngram'].isin(entries)].freq.sum()
+    d = d[~d['ngram'].isin(entries)]
+    return d
+
+
+def gather_and_clean(lang, n):
+    
+    global max_min_freq_per_file
+    max_min_freq_per_file = 0
+
+    global freq_words_added
+    freq_words_added = 0
+
+    global freq_words_removed
+    freq_words_removed = 0
+
+    # dictionary of dataframes to hold ngrams to be added at other n
+    df_empty = pd.DataFrame({'ngram' : [], 'freq' : []})
+    dother = {key: df_empty for key in range(1, 11)}
+
+    
+    # gather ngrams
+    d = gather_per_gz_files(lang, n)
+
+    
+    # remove part-of-speech tags
+    d = clean_remove_pos_tags(lang, n, d)
+
+
+    # split contractions
+
+    if lang == 'french':
+
+        d, dother = split_contractions(d, dother)
+
+        ## add the parts of the 2-grams resulting from split back as 1-grams
+
+        if n == 1:
+            freq_words_added += sum(dother[2].freq)
+            dadd = split_2grams_to_1grams(dother[2])
+            d = pd.concat([d, dadd], axis=0)
+            d = d.groupby("ngram")[["freq"]].sum()
+            d.reset_index(level=0, inplace=True)
+            d = d.sort_values(by=['freq'], ascending=False).reset_index(drop=True)            
+            dother[2] = df_empty
+
+
+    # handle entries ending with "_"
+
+    d = replace_pattern_and_group(d, pattern=r"_$", replacement="")
+
+
+    # merge upcase and lowcase
+
+    d = merge_upcase_lowcase(d, 0.92)
+
+
+    # save copy of data before starting to remove entries
+    dfull = d
+
+
+    # remove entries with only punctuation and numbers
+
+    punctuation_and_numbers_regex = r"^[ _\W0-9]+$"
+    d = remove_pattern(d, punctuation_and_numbers_regex)
+
+
+    # handle uppercase words
+
+    upcases_to_keep = {key: [] for key in all_langs}
+    upcases_to_keep['german'] = ["DDR", "AG", "BRD"]
+    upcases_to_keep['english'] = ["a", "I", "God", "American", "English", "Jesus", "British", "European",
+                                  "America", "French", "China", "German", "Europe", "Christ", "England",
+                                  "Chrstian", "Bible", "June", "Chinese", "India", "July", "African",
+                                  "April", "January", "September", "Indian", "December", "Africa",
+                                  "October", "August", "Germany", "Israel", "November", "February",
+                                  "Americans", "UK", "California", "Jewish", "Japan", "Canada",
+                                  "Japanese", "Britain", "Greek", "Roman", "Russian", "Spanish"]
+    upcases_to_keep['english-fiction'] = upcases_to_keep['english']
+    upcases_to_keep['french'] = ["France", "Europe", "Afrique", "Allemagne", "Londres", "Angleterre",
+                                 "Italie", "Amérique", "Chine", "Espagne", "Israël", "Russie", "Noël",
+                                 "Canada", "Orient", "Bretagne", "Bruxelles", "Algérie", "Inde", "Asie",
+                                 "Belgique", "Égypte", "Occident", "Japon", "Congo", "Maroc", "Moscou",
+                                 "Autriche", "Brésil", "Venise", "Pologne", "Platon", "Moïse", "Aristote",
+                                 "Cameroun" "Turquie", "Provence", "Sénégal", "Méditerranée", "Mexique",
+                                 "Syrie", "American", "Australie", "Athènes", "Alpes", "Suède", "Grèce",
+                                 "Normandie", "Tunisie", "Liban", "Socrate", "Hongrie", "Alsace", "Guinée",
+                                 "Rhin", "Californie", "Palestine", "Indes", "Barcelone", "Danemark",
+                                 "Munich", "Arabie", "Norvège", "Roumanie", "Lénine", "Maghreb", "Indochine",
+                                 "Beyrouth", "Pérou", "Finlande", "Libye", "Soudan", "Colombie", "Haïti",
+                                 "URSS", "ADN", "ONU",
+                                 "'S"]
+    # d[d.ngram.str.contains(r"[A-ZÀ-Ü]")].to_csv(f"{tmp_path(lang)}/ngrams_{n}_tmp.csv", index=False)
+    # checked for for words to keep up to entry 1000
+
+    if n == 1:
+        if lang in ['german']:
+            upcase_regex = r"^[A-ZÀ-Ü]+$"
+        else:
+            upcase_regex = r"[A-ZÀ-Ü]"            
+        d = remove_pattern(d, upcase_regex, upcases_to_keep[lang])
+
+
+    # remove most one-character words
+
+    onechars_to_keep = {key: [] for key in all_langs}
+    onechars_to_keep['french'] = ["à", "a", "y"]
+    onechars_to_keep['russian'] = ["а", "б", "в", "ж", "и", "к", "о", "с", "у", "я"]
+
+    if lang != 'chinese_simplified':        
+        onechar_regex = r"^.$"
+        d = remove_pattern(d, onechar_regex, onechars_to_keep[lang])
+
+
+    # remove contractions for some langagues
+    # TODO remove for other languages too?
+    if lang in ['german', 'russian']:
+        d[d.ngram.str.contains(r"'")]
+        d = remove_pattern(d, r"'")
+
+
+    # remove entries with non-word characters other than "'" and " "
+    # TODO this needs to be changed, especially for n-grams with n > 1
+    if lang == 'russian':
+        nonword_regex = r"[^\w' -,]"
+    else:
+        nonword_regex = r"[^\w' ,]"
+        d = remove_pattern(d, nonword_regex)
+
+
+    # remove entries with numbers
+    d = remove_pattern(d, r"[0-9]")
+
+
+    # manually remove any remaining unwanted words 
+    # e.g. names of persons, wrong language words, some abbrevations without a dot
+
+    words_to_remove = {key: [] for key in all_langs}
+    words_to_remove['german'] = \
+        ["of", "'s", "et", "al", "to", "la", "Peter", "New", "is", "Rn", "Thomas", "Paul", 
+         "for", "on", "York", "Anna", "Maria", "London", "that", "be", "en", "David", "le",
+         "Georg", "Franz", "Tom", "Johann", "cm", "mm", "it", "Daniel", "Christian", "Frank", 
+         "Richard", "he", "di", "by", "Walter", "kg", "Alexander", "Klaus", "fie", "Otto",
+         "men", "social", "ift", "fo", "Alex", "Werner", "no", "Jack", "ge", "se", "James", 
+         "Hermann", "at", "you", "Sam", "mg", "Goethe", "Jan", "Carl", "Jean", "from", "or",
+         "Rhodan", "Miss", "kg", "Julia", "ver", "les", "Kant", "el", "Eva", "non", "Adam", 
+         "do"]
+    # checked until word 3222
+
+    d = remove_entries(d, words_to_remove[lang])
+
+
+    # get dataframe of removed words
+    drem = dfull[~dfull.ngram.isin(d.ngram)]
+
+    
+    d = d.sort_values(by=['freq'], ascending=False).reset_index(drop=True)
+    check_if_too_much_truncated(lang, n, d)
+    d[:number_of_most_freq[lang][n]].to_csv(f"ngrams/{n}grams_{lang}.csv", index=False)
+
+    drem = drem.sort_values(by=['freq'], ascending=False).reset_index(drop=True)
+    drem = drem[drem.freq >= d.iloc[number_of_most_freq[lang][n]-1, 1]]
+    drem.to_csv(f"ngrams/more/{lang}/{n}grams_{lang}_2_removed.csv", index=False)
+
+
+def gather_and_clean_all():
 
     for lang in langs:
 
@@ -156,16 +476,16 @@ def gather_and_clean():
 
             print("n:", n)
 
-            global max_min_freq_per_file
-            max_min_freq_per_file = 0
-
-            d = gather_per_gz_files(lang, n)
-            clean_remove_pos_tags(lang, n, d)
+            gather_and_clean(lang, n)
+            
 
 
 ###############################################################################
 # run
 
 if __name__ == '__main__':
-    gather_and_clean()
+gather_and_clean_all()
+
+
+###############################################################################
 
